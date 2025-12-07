5212c145b98f83befb81ae9af6f5a0a8d25a3915
Matheus-ESW <matheussoaresramos@gmail.com>
2025-12-07 13:31:22 -0300

    Ajustes

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Teste!!")
+def lambda_handler():
+    print("Função executada a cada 10 minutos.")
+    return {
+        'statusCode': 200,
+        'body': 'Execução bem-sucedida.'
+    }
+
+lambda_handler()
